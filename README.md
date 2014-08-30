682747aa960344faa2fbe2656f939b92db502db4
Eric Heep <ericheep@gmail.com>
2014-08-29 23:55:42 -0500

    added a basic function with Dr. Mario colors

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-Jellybones
+jellybones
 ===============
 
 A quick project for 2013's New Years, to be improved for the coming year.
 
-Requirements
+requirements
 ============
 - [TLC 5940 library](https://code.google.com/p/tlc5940arduino/) - edit
   `tlc_config.h` and edit `NUM_TLCS` macro: `#define NUM_TLCS    4`
--- jellybones.ino
+++ jellybones.ino
@@ -12,27 +12,13 @@ int _set_all_led_num = 0;
 
 #define NUM_LEDS 21
 int LEDS[NUM_LEDS][3] = {
-  {0, 1, 2 },
-  {3, 4, 5 },
-  {6, 7, 8  },
-  {9, 10, 11  },
-  {12, 13, 14  },
-  {16, 17, 18  },
-  {19, 20, 21  },
-  {22, 23, 24  },
-  {25, 26, 27  },
-  {28, 29, 30  },
-  {31, 32, 33  },
-  {34, 35, 36  },
-  {37, 38, 39  },
-  {40, 41, 42  },
-  {43, 44, 45  },
-  {46, 47, 48  },
-  {49, 50, 51  },
-  {52, 53, 54  },
-  {55, 56, 57  },
-  {58, 59, 60  },
-  {63, 62, 61  },
+  {0, 1, 2 }, {3, 4, 5 }, {6, 7, 8  },
+  {9, 10, 11  }, {12, 13, 14  }, {16, 17, 18  },
+  {19, 20, 21  }, {22, 23, 24  }, {25, 26, 27  },
+  {28, 29, 30  }, {31, 32, 33  }, {34, 35, 36  },
+  {37, 38, 39  }, {40, 41, 42  }, {43, 44, 45  },
+  {46, 47, 48  }, {49, 50, 51  }, {52, 53, 54  },
+  {55, 56, 57  }, {58, 59, 60  }, {63, 62, 61  },
 };
 
 
@@ -314,6 +300,20 @@ void walkDrop() {
   }
 }
 
+// Dr. Mario colors
+// red, yellow, blue
+int ryb[3] = {0, 60, 240};
+
+void redYelBlu() {
+  Tlc.clear();
+  int times = random(80, 400);
+  for (int i = 0; i < times; i++) {
+    HSV hsv = {ryb[random(0,2)], 1, 1 };
+    setColor(random(0, NUM_LEDS), hsv);
+    Tlc.update();
+    delay(random(50,100));
+  }
+}
 
 // setup and loop
 void setup() {
@@ -323,10 +323,11 @@ void setup() {
 
 void loop() {
   //tester();
-  patches();
-  single();
-  walkDrop();
-  randomPatches();
-  cyclingGradients();
-  doubleRainbow();
+  redYelBlu();
+  //patches();
+  //single();
+  //walkDrop();
+  //randomPatches();
+  //cyclingGradients();
+  //doubleRainbow();
 }
